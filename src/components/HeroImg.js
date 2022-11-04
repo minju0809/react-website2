a802500a789c6f8518ead26c20fd984a0aa751c9
minju0809 <loverminju88@naver.com>
2022-11-04 21:30:40 +0900

    HeroImg 추가

---
+++ src/components/HeroImg.js
@@ -0,0 +1,24 @@
+import "./HeroImgStyles.css";
+import React from "react";
+import IntroImg from "../assets/노트북.jpeg";
+import { Link } from "react-router-dom";
+
+const HeroImg = () => {
+  return (
+    <div className="hero">
+      <div className="mask">
+        <img className="into-img" src={IntroImg} alt="IntroImg" />
+      </div>
+      <div className="content">
+        <p>HI, I'M A FREELANCER.</p>
+        <h1>React Developer.</h1>
+        <div>
+          <Link to="/project" className="btn">PROJECTS</Link>
+          <Link to="/contact" className="btn btn-light">CONTACT</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroImg;
