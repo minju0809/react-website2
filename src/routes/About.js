0262e650054343c32fbe0d0aa5d6d888291a7475
minju0809 <loverminju88@naver.com>
2022-11-08 21:46:02 +0900

    about 추가

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import HeroImg2 from '../components/HeroImg2';
+import HeroImg2 from "../components/HeroImg2";
+import AboutContent from "../components/AboutContent";
 
 const About = () => {
-  return <div>
-    <Navbar />
-    <HeroImg2 heading="ABOUT." text="Im a friendly Front-End Developer." />
-    <Footer />
-  </div>;
-}
+  return (
+    <div>
+      <Navbar />
+      <HeroImg2 heading="ABOUT." text="Im a friendly Front-End Developer." />
+      <AboutContent />
+      <Footer />
+    </div>
+  );
+};
 
-export default About
+export default About;
