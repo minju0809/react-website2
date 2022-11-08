c14ea93bb12607a7785b4ea4be9c4e0dd0c1a54b
minju0809 <loverminju88@naver.com>
2022-11-08 20:56:51 +0900

    가격, 프로젝트 카드 생성

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,26 @@
+import pro1 from "../assets/image1.jpeg";
+import pro2 from "../assets/image2.jpeg";
+import pro3 from "../assets/image4.jpeg";
+
+const ProjectCardData = [
+  {
+    imgsrc: pro1,
+    title: "Ecomerce Website Design",
+    text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tangible and self-affirming sense of 'Wow! I really can do this' for over 100,000 online entrepreneurs. Best-selling marketing courses",
+    view: "프로젝트 링크",
+  },
+  {
+    imgsrc: pro2,
+    title: "LMS/Course Website Design",
+    text: "Podcast Facebook community and top-ranked podcast, Amy inspires a grounded, tangible and self-affirming sense of 'Wow! I really can do this' for over 100,000 online entrepreneurs. Best-selling marketing courses",
+    view: "프로젝트 링크",
+  },
+  {
+    imgsrc: pro3,
+    title: "Rymoe Ecommerce Design",
+    text: "Top-ranked Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tangible and self-affirming sense of 'Wow! I really can do this' for over 100,000 online entrepreneurs. Best-selling marketing courses",
+    view: "프로젝트 링크",
+  },
+];
+
+export default ProjectCardData;
